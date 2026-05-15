0e3ff77db882aaf91531a925d89b3b4c9f414a00
Anton Smirnov <antony01s@mail.ru>
2026-05-15 19:16:06 +0300

    feat: rewrite Collector as Kafka consumer/producer with subscription refresh timer

--- services/collector/cmd/collector/main.go
+++ services/collector/cmd/collector/main.go
@@ -1,55 +1,92 @@
 package main
 
 import (
+	"context"
 	"log"
-	"net"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 
-	pb "github.com/LuhTonkaYeat/GoHomeworks/services/collector/api/proto/collector"
 	"github.com/LuhTonkaYeat/GoHomeworks/services/collector/internal/adapter/github"
+	"github.com/LuhTonkaYeat/GoHomeworks/services/collector/internal/adapter/kafka"
 	"github.com/LuhTonkaYeat/GoHomeworks/services/collector/internal/adapter/subscribe"
-	grpcHandler "github.com/LuhTonkaYeat/GoHomeworks/services/collector/internal/delivery/grpc"
 	"github.com/LuhTonkaYeat/GoHomeworks/services/collector/internal/usecase"
-	"google.golang.org/grpc"
 )
 
 func main() {
 	subscribeAddr := os.Getenv("SUBSCRIBE_ADDR")
 	if subscribeAddr == "" {
 		subscribeAddr = "subscribe:50053"
-		log.Printf("SUBSCRIBE_ADDR not set, using default: %s", subscribeAddr)
 	}
-
+	
 	subscribeClient, err := subscribe.NewClient(subscribeAddr)
 	if err != nil {
 		log.Fatalf("Failed to create Subscribe client: %v", err)
 	}
 	defer subscribeClient.Close()
-
+	
 	githubClient := github.NewClient()
-
-	repoUseCase := usecase.NewRepositoryUseCase(githubClient, subscribeClient)
-
-	grpcServer := grpcHandler.NewServer(repoUseCase)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "50051"
+	
+	kafkaBrokers := strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
+	if len(kafkaBrokers) == 0 || kafkaBrokers[0] == "" {
+		kafkaBrokers = []string{"localhost:9092"}
 	}
-
-	listener, err := net.Listen("tcp", ":"+port)
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+	
+	requestsTopic := os.Getenv("KAFKA_REQUESTS_TOPIC")
+	if requestsTopic == "" {
+		requestsTopic = "repository.fetch.requests"
 	}
-
-	server := grpc.NewServer()
-	pb.RegisterCollectorServiceServer(server, grpcServer)
-
-	log.Printf("Collector server started on port %s", port)
+	
+	responsesTopic := os.Getenv("KAFKA_RESPONSES_TOPIC")
+	if responsesTopic == "" {
+		responsesTopic = "repository.fetch.responses"
+	}
+	
+	kafkaProducer := kafka.NewProducer(kafkaBrokers, responsesTopic)
+	defer kafkaProducer.Close()
+	
+	collectorUseCase := usecase.NewCollectorUseCase(githubClient, subscribeClient, kafkaProducer)
+	
+	kafkaConsumer := kafka.NewConsumer(kafkaBrokers, requestsTopic, "collector-group", func(request kafka.RepositoryFetchRequest) error {
+		return collectorUseCase.ProcessFetchRequest(context.Background(), request)
+	})
+	
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	
+	kafkaConsumer.Start(ctx)
+	defer kafkaConsumer.Close()
+	
+	ticker := time.NewTicker(15 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				log.Println("Stopping refresh timer...")
+				return
+			case <-ticker.C:
+				log.Println("Timer triggered: refreshing subscriptions...")
+				if err := collectorUseCase.RefreshSubscriptions(context.Background()); err != nil {
+					log.Printf("Error refreshing subscriptions: %v", err)
+				}
+			}
+		}
+	}()
+	
+	log.Println("Collector service started")
 	log.Printf("Subscribe address: %s", subscribeAddr)
-	log.Println("Waiting for gRPC requests...")
-
-	if err := server.Serve(listener); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
-	}
+	log.Printf("Kafka brokers: %v", kafkaBrokers)
+	log.Printf("Requests topic: %s", requestsTopic)
+	log.Printf("Responses topic: %s", responsesTopic)
+	log.Println("Waiting for Kafka messages...")
+	
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	
+	log.Println("Shutting down...")
+	ticker.Stop()
+	cancel()
 }
